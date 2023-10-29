f43bc14a906eef176a8cd5a6e5a927f9e927a401
zhitianguan <zguan.university@gmail.com>
2023-10-29 14:43:41 -0400

    Complete Bootstrap and SQLAlchemy sections

---
+++ create_db.py
@@ -0,0 +1,10 @@
+from project.app import app, db
+from project.models import Post
+
+
+with app.app_context():
+    # create the database and the db table
+    db.create_all()
+
+    # commit the changes
+    db.session.commit()
