56c94f8bad437665252de6aad0bb7d7f64ae0b3f
zhitianguan <zguan.university@gmail.com>
2023-10-29 14:25:40 -0400

    Completed add some style and JavaScript sections

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pytest
 from pathlib import Path
@@ -72,3 +73,9 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get('/delete/1')
+    data = json.loads(rv.data)
+    assert data["status"] == 1
